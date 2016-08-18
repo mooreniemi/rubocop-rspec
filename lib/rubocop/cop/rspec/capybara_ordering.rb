f408c7263c614aa7906781e8a13a56ca7ba5067f
Alex Moore-Niemi <alex.mooreniemi@codeship.com>
2016-08-18 16:45:17 -0400

    tidying up

--- lib/rubocop/cop/rspec/capybara_ordering.rb
+++ lib/rubocop/cop/rspec/capybara_ordering.rb
@@ -29,10 +29,10 @@ module RuboCop
         include RuboCop::RSpec::SpecOnly,
           RuboCop::RSpec::Language
 
-        MSG = 'To maintain Capybara concurrency protection, swap the line sequence of `%{one}` and `%{two}`.'.freeze
+        MSG = 'To maintain Capybara concurrency protection,' \
+          ' swap the line sequence of `%{one}` and `%{two}`.'.freeze
 
         SAFE_MATCHERS = Capybara::RSpecMatchers.instance_methods(false).freeze
-        # expect is used by both so we don't care
         UNSAFE_MATCHERS = ::RSpec::Matchers.instance_methods(false).
           tap {|h| h.delete(:expect) }.freeze
         ALL_MATCHERS = SAFE_MATCHERS + UNSAFE_MATCHERS
@@ -66,9 +66,9 @@ module RuboCop
           two = matchers.next
 
           lambda do |corrector|
-            #require'pry';binding.pry
             safe = two.parent
             unsafe = one.parent
+
             corrector.replace(unsafe.loc.expression, safe.source)
             corrector.replace(safe.loc.expression, unsafe.source)
           end
